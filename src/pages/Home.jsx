fd6a797fffd8376822ed0e11f426f200803a40a6
Phillip Yoder <phillipmyoder@gmail.com>
2021-04-03 15:37:55 -0400

    useState Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import '../App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import Rushia from './images/rushia.png';
 import Coco from './images/coco.png';
 import Matsuri from './images/matsuri.png';
@@ -11,33 +11,24 @@ import Suisei from './images/suisei.png';
 import Choco from './images/choco.png';
 import Subaru from './images/subaru.png';
 
-var correctList = ["Kanata", "Matsuri", "Rushia", "Shion", "Suisei" ];
+//var correctList = ["Kanata", "Matsuri", "Rushia", "Shion", "Suisei" ];
 
 function Home (){
-      function compareLists () {
-        var form = document.getElementById("wall"),
-        inputs = form.getElementsByTagName("input");
-        var testList = [];
-        
-        for (var i = 0, max = inputs.length; i < max; i++){
-          if(inputs[i].type === "checkbox" && inputs[i].checked){
-            testList.push(inputs[i].id);
-          }
-        }
+  const [rushia, setRushia] = useState(false);
+  const [noel, setNoel] = useState(false);
+  const [shion, setShion] = useState(false);
 
-        var sortedArrList = testList.sort();
-        var correct = 0;
-        for(var j = 0, max2 = testList.length; j < max2; j++){
-          console.log(sortedArrList[j] + " " + correctList[j]);
-          if(sortedArrList[j] === correctList[j]){
-            correct++;
-          }
-          
-          else{
-            correct --;
-          }
-        }
-        if(correct === 5)
+  const [coco, setCoco] = useState(false);
+  const [lamy, setLamy] = useState(false);
+  const [suisei, setSuisei] = useState(false);
+
+  const [matsuri, setMatsuri] = useState(false);
+  const [kanata, setKanata] = useState(false);
+  const [choco, setChoco] = useState(false);
+  
+      function compareLists () {
+        if(rushia && matsuri && kanata && shion && suisei 
+          && !noel && !coco && !lamy && !choco)
         {
           alert("Correct!");
           window.location = "/Pages";
@@ -49,45 +40,58 @@ function Home (){
         //console.log(e.target.id);
       }
 
-      function mark(el){
-        var form = document.getElementById("wall"),
-        inputs2 = form.getElementsByTagName("input");
-        
-        var ids = el.target.id;
-        //console.log(inputs2);
-        //inputs[ids].className = "selected";
-        console.log("Selected");
-        if(inputs2[ids].checked === false){
-          document.getElementById(ids).style.border = "1px solid blue";
-        }
-        else{
-          document.getElementById(ids).style.border = "1px solid black";
-        }
-      }
-
     return(
         <div className = "test">
           <form id = "wall">
-          <input type = "checkbox" id = "Rushia" className = "checkbox-gone" />
-          <label htmlFor = "Rushia"><img src = {Rushia} id = "0" className = "row-1" alt = "Rushia" onClick={(e) => mark(e)} /></label>
-          <input type = "checkbox" id = "Noel" className = "checkbox-gone" />
-          <label htmlFor = "Noel"><img src = {Noel} id = "1" className = "row-1" alt = "Noel" onClick={(e) => mark(e)} /></label>
-          <input type = "checkbox" id = "Shion" className = "checkbox-gone" />
-          <label htmlFor = "Shion"><img src = {Shion} id = "2" className = "row-1" alt = "Shion" onClick={(e) => mark(e)} /></label>
           
-          <input type = "checkbox" id = "Coco" className = "checkbox-gone" />
-          <label htmlFor = "Coco"><img src = {Coco} id = "3" className = "row-2" alt = "Coco" onClick={(e) => mark(e)} /></label>
-          <input type = "checkbox" id = "Lamy" className = "checkbox-gone" />
-          <label htmlFor = "Lamy"><img src = {Lamy} id = "4" className = "row-2" alt = "Lamy" onClick={(e) => mark(e)} /></label>
-          <input type = "checkbox" id="Suisei" className = "checkbox-gone" />
-          <label htmlFor = "Suisei"><img src = {Suisei} id = "5" className = "row-2" alt = "Suisei" onClick={(e) => mark(e)} /></label>
+          <img 
+          src = {Rushia} 
+          className = {"row-1 " + (rushia ? "selected" : " ")}
+          alt = "Rushia" 
+          onClick={() => setRushia(!rushia)} />
           
-          <input type = "checkbox" id = "Matsuri" className = "checkbox-gone" />
-          <label htmlFor = "Matsuri"><img src = {Matsuri} id = "6" className = "row-3" alt = "Matsuri" onClick={(e) => mark(e)} /></label>
-          <input type = "checkbox" id = "Kanata" className = "checkbox-gone" />
-          <label htmlFor = "Kanata"><img src = {Kanata} id = "7" className = "row-3" alt = "Kanata" onClick={(e) => mark(e)} /></label>
-          <input type = "checkbox" id = "Choco" className = "checkbox-gone" />
-          <label htmlFor = "Choco"><img src = {Choco} id = "8" className = "row-3" alt = "Choco" onClick={(e) => mark(e)} /></label>
+          <img src = {Noel} 
+          className = {"row-1 " + (noel ? "selected" : " ")}
+           alt = "Noel" 
+           onClick={() => setNoel(!noel)} />
+
+          <img src = {Shion} 
+          className = {"row-1 " + (shion ? "selected" : " ")}
+          alt = "Shion" 
+          onClick={() => setShion(!shion)} />
+          
+          
+          <img src = {Coco}
+          className = {"row-2 " + (coco ? "selected" : " ")}
+          alt = "Coco" 
+          onClick={() => setCoco(!coco)} />
+
+          <img src = {Lamy} 
+          className = {"row-2 " + (lamy ? "selected" : " ")}
+          alt = "Lamy" 
+          onClick={() => setLamy(!lamy)} />
+          
+          <img src = {Suisei} 
+          className = {"row-2 " + (suisei ? "selected" : " ")}
+          alt = "Suisei" 
+          onClick={() => setSuisei(!suisei)} />
+          
+          
+          <img src = {Matsuri}
+          className = {"row-3 " + (matsuri ? "selected" : " ")}
+          alt = "Matsuri" 
+          onClick={() => setMatsuri(!matsuri)} />
+
+          
+          <img src = {Kanata} 
+          className = {"row-3 " + (kanata ? "selected" : " ")}
+          alt = "Kanata" 
+          onClick={() => setKanata(!kanata)} />
+          
+          <img src = {Choco} 
+          className = {"row-3 " + (choco ? "selected" : " ")}
+          alt = "Choco" 
+          onClick={() => setChoco(!choco)} />
           
           
           </form>
